b9adb5ed63c9690098cbb31219701cfef69bbfb3
karan <karanraj17111996@gmail.com>
2020-08-26 15:37:01 +0530

    added custom error handling middleware

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true, useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
         useFindAndModify: false,
         useCreateIndex: true
     });
